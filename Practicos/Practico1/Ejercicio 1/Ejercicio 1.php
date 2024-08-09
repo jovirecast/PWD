6ef6aad93a8e3fa4cd7e543ac6a887b4541b69cd
jovirecast <jreyescastello@gmail.com>
2024-08-09 19:17:50 -0300

    comienzo TP 1

---
+++ Practicos/Practico1/Ejercicio 1/Ejercicio 1.php
@@ -0,0 +1,12 @@
+<?php
+
+/* 
+Trabajo práctico 1 - Repaso
+Programación Web Dinámica - año 2024 
+*/
+
+// Ejercicio 1
+
+$variable = 1;
+
+echo "La variable creada tiene el valor: " . $variable . "\n";
---
+++ Practicos/Practico1/Ejercicio 2/Ejercicio 2.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Ejercicio 2</title>
+</head>
+
+<body>
+    <?php
+
+    /* 
+    Trabajo práctico 1 - Repaso
+    Programación Web Dinámica - año 2024 
+    */
+
+    // Ejercicio 2
+
+    $nombre = "Homero";
+    $apellido = "Simpson";
+    $edad = "treinta y cuatro";
+    $direccion = "Springfield";
+
+    echo "Nombre: " . strtoupper($nombre) . "<br>" . 
+    "Apellido: " . strtoupper($apellido) . "<br>" . 
+    "Edad: " . strtoupper($edad) . "<br>" . 
+    "Dirección: " . strtoupper($direccion);
+
+    ?>
+</body>
+
+</html>
