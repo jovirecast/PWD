bada80d5222288fc02400fd3966cd11efce3110d
jovirecast <jreyescastello@gmail.com>
2024-08-18 21:28:11 -0300

    ej1 tp3 terminado

--- Practicos/TP3/Vista/Action/actionEJ1.php
+++ Practicos/TP3/Vista/Action/actionEJ1.php
@@ -19,9 +19,9 @@ $respuesta = $obj->upload($datos);
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Ejercicio 1 TP1</title>
     <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.5/dist/jquery.validate.min.js"></script>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.5/dist/jquery.validate.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </head>
 
 <body>
@@ -39,9 +39,11 @@ $respuesta = $obj->upload($datos);
             ?>
         </div>
         <div>
-            <div class="btn-group-vertical m-2" role="group" aria-label="">
-                <a name="volver" id="volver" class="btn btn-primary" href="" role="button">Descargue su archivo <span><?php echo $datos['archivo']["name"] ?></span> subido</a>
-            </div>
+            <?php
+            echo '<div class="btn-group-vertical m-2" role="group" aria-label="">
+                <a name="volver" id="volver" class="btn btn-primary" href="' . '../../Modelo/archivos/' . $datos['archivo']['name'] . '" role="button">Descargue su archivo <span>' . $datos['archivo']["name"] . '</span> subido</a>
+            </div>';
+            ?>
         </div>
         <div class="btn-group-vertical m-2" role="group" aria-label="">
             <a name="volver" id="volver" class="btn btn-primary" href="../EJ1.php" role="button">Regresar</a>
